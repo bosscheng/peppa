8a0f4fd07678d60736f8951b281964680fccc6bc
bosscheng <25022549@qq.com>
2019-01-23 15:12:20 +0800

    add files
    add files

--- test/controller/test.js
+++ test/controller/test.js
@@ -1,6 +1,4 @@
 module.exports = async function(ctx) {
-    // let t = await ctx.sonicRequest.get('http://www.suning.com');
-    // console.log(t);
     console.log('test controller');
     return ctx.render('test/parent');
 };
